d1f2a04d6aecff3290618b7befdc5f495d1cb0b3
James <jamespatterson1985@yahoo.com>
2022-12-03 10:58:56 -0500

    Add Store class

---
+++ models/Store.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurant_API.models
+{
+    public class Store
+    {
+        public int StoreId { get; set; }
+        public string StoreName { get; set; }
+        public string Address1 { get; set; }
+        public string? Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int PostalCode { get; set; }
+    }
+}
