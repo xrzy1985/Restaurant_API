b6b1048553176fd691a5b538f089385c72b51e54
James <jamespatterson1985@yahoo.com>
2022-12-03 12:10:47 -0500

    add if statement to catch error in query to db for stores

--- Controllers/StoresController.cs
+++ Controllers/StoresController.cs
@@ -26,47 +26,54 @@ namespace Restaurant_API.Controllers
             {
                 DataTable dataTable = new Query(storesSql, _config).GetDataTable();
                 List<Store> stores = new List<Store>();
-                for (int i = 0; i < dataTable.Rows.Count; i++)
+                if (dataTable != null)
                 {
-                    DataRow data = dataTable.Rows[i];
-                    string hoursSql = $"select * from storeHours where storeId = {data["storeId"]}";
-                    DataTable storeHoursDataTable = new Query(hoursSql, _config).GetDataTable();
-                    Dictionary<string, List<string>> hours = new Dictionary<string, List<string>>();
-                    for (int j = 0; j < storeHoursDataTable.Rows.Count; j++)
+                    for (int i = 0; i < dataTable.Rows.Count; i++)
                     {
-                        DataRow storeHoursData = storeHoursDataTable.Rows[j];
-                        if (hours.ContainsKey(Convert.ToString(storeHoursData["dayOfWeek"])))
+                        DataRow data = dataTable.Rows[i];
+                        string hoursSql = $"select * from storeHours where storeId = {data["storeId"]}";
+                        DataTable storeHoursDataTable = new Query(hoursSql, _config).GetDataTable();
+                        Dictionary<string, List<string>> hours = new Dictionary<string, List<string>>();
+                        for (int j = 0; j < storeHoursDataTable.Rows.Count; j++)
                         {
-                            List<string> existingList = new List<string>(hours[Convert.ToString(storeHoursData["dayOfWeek"])]);
-                            string newHours = Convert.ToString(storeHoursData["hours"]);
-                            if (!string.IsNullOrEmpty(newHours))
+                            DataRow storeHoursData = storeHoursDataTable.Rows[j];
+                            if (hours.ContainsKey(Convert.ToString(storeHoursData["dayOfWeek"])))
                             {
-                                existingList.Add(Convert.ToString(storeHoursData["hours"]));
+                                List<string> existingList = new List<string>(hours[Convert.ToString(storeHoursData["dayOfWeek"])]);
+                                string newHours = Convert.ToString(storeHoursData["hours"]);
+                                if (!string.IsNullOrEmpty(newHours))
+                                {
+                                    existingList.Add(Convert.ToString(storeHoursData["hours"]));
+                                }
+                                hours[Convert.ToString(storeHoursData["dayOfWeek"])] = existingList;
                             }
-                            hours[Convert.ToString(storeHoursData["dayOfWeek"])] = existingList;
-                        } else
-                        {
-                            List<string> newList = new List<string>();
-                            string newHours = Convert.ToString(storeHoursData["hours"]);
-                            if (!string.IsNullOrEmpty(newHours))
+                            else
                             {
-                                newList.Add(Convert.ToString(storeHoursData["hours"]));
+                                List<string> newList = new List<string>();
+                                string newHours = Convert.ToString(storeHoursData["hours"]);
+                                if (!string.IsNullOrEmpty(newHours))
+                                {
+                                    newList.Add(Convert.ToString(storeHoursData["hours"]));
+                                }
+                                hours[Convert.ToString(storeHoursData["dayOfWeek"])] = newList;
                             }
-                            hours[Convert.ToString(storeHoursData["dayOfWeek"])] = newList;
                         }
+                        stores.Add(new Store(
+                            Convert.ToInt16(data["storeId"]),
+                            Convert.ToString(data["storeName"]),
+                            Convert.ToString(data["address1"]),
+                            Convert.ToString(data["address2"]),
+                            Convert.ToString(data["city"]),
+                            Convert.ToString(data["state"]).ToUpper(),
+                            Convert.ToInt16(data["postalCode"]),
+                            hours
+                        ));
                     }
-                    stores.Add(new Store(
-                        Convert.ToInt16(data["storeId"]),
-                        Convert.ToString(data["storeName"]),
-                        Convert.ToString(data["address1"]),
-                        Convert.ToString(data["address2"]),
-                        Convert.ToString(data["city"]),
-                        Convert.ToString(data["state"]).ToUpper(),
-                        Convert.ToInt16(data["postalCode"]),
-                        hours
-                    ));
+                    return new Response(200, stores);
+                } else
+                {
+                    return new Error(400, "There was an issue fetching the stores data.");
                 }
-                return new Response(200, stores);
             } catch (Exception ex)
             {
                 return new Error(500, ex.Message);
