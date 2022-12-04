908e89f69b382b951206f59e7ae87190bde090ae
James <jamespatterson1985@yahoo.com>
2022-12-04 10:34:27 -0500

    Add dispositions class

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ GET					: /api/users
 GET, DELETE			: /api/user/:uuid  
 POST, PUT			: /api/user  
 GET					: /api/stores  
+GET					: /api/stores/dispositions/:storeId  
 GET					: /api/store/:storeId  
---
+++ models/Disposition.cs
@@ -0,0 +1,20 @@
+﻿namespace Restaurant_API.models
+{
+    public class Disposition
+    {
+        public string StoreId { get; set; }
+        public Boolean Curbside { get; set; }
+        public Boolean Delivery { get; set; }
+        public Boolean Pickup { get; set; }
+
+        public Disposition() { }
+
+        public Disposition(string storeId, bool curbside, bool delivery, bool pickup)
+        {
+            StoreId = storeId;
+            Curbside = curbside;
+            Delivery = delivery;
+            Pickup = pickup;
+        }
+    }
+}
