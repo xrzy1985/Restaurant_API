efa17a6341692d742664a3e14df501ad566d551b
James <jamespatterson1985@yahoo.com>
2022-12-03 23:35:45 -0500

    Finish the put request, change get to use uuid, and add initial delete request for user

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -18,13 +18,12 @@ namespace Restaurant_API.Controllers
             _config = config;
         }
 
-        [HttpGet("{id}")]
-        public ActionResult<object> GetUser(int id)
+        [HttpGet("{uuid}")]
+        public ActionResult<object> GetUser(string uuid)
         {
             try
             {
-                DataTable dataTable = new GetQuery($"select uuid, name, email, dob from users where id = {id}", _config).GetDataTable();
-
+                DataTable dataTable = new GetQuery($"select uuid, name, email, dob from users where uuid = '{uuid}'", _config).GetDataTable();
                 if (dataTable != null)
                 {
                     DataRow data = dataTable.Rows[0];
@@ -36,7 +35,7 @@ namespace Restaurant_API.Controllers
                     ));
                 } else
                 {
-                    return new ErrorResponse(400, "There was an error fetching the data.");
+                    return new ErrorResponse(404, "There was an error fetching the data.");
                 }
             } catch(Exception ex)
             {
@@ -72,7 +71,7 @@ namespace Restaurant_API.Controllers
                 connection.Close();
                 return new Dictionary<string, object>()
                 {
-                    { "status", 200 },
+                    { "status", 201 },
                     { "message", "The user was created successfully." }
                 };
             }
@@ -80,6 +79,55 @@ namespace Restaurant_API.Controllers
             {
                 return new ErrorResponse(500, ex.Message);
             }
+        }
+
+        [HttpPut]
+        public ActionResult<object> PutUser(ExistingUser user)
+        {
+            try
+            {
+                DataTable dataTable = 
+                    new GetQuery($"select uuid, name, email, dob from users where uuid = '{user.Uuid}';", _config).GetDataTable();
+                DataRow data = dataTable.Rows[0];
+                if (data != null)
+                {
+                    string updateSqlString = $"update users set name=@Name,email=@Email,dob=@Dob WHERE uuid='{user.Uuid}';";
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    SqlConnection connection = new SqlConnection(_config.GetConnectionString("Restaurant").ToString());
+                    SqlCommand command = new SqlCommand();
+                    connection.Open();
+                    command = new SqlCommand(updateSqlString, connection);
+                    command.Parameters.Add("@Name", SqlDbType.VarChar, 50).Value = user.Name;
+                    command.Parameters.Add("@Email", SqlDbType.VarChar, 50).Value = user.Email;
+                    command.Parameters.Add("@Dob", SqlDbType.VarChar, 50).Value =
+                        DateTime.Parse(DateOnly.FromDateTime(user.Dob).ToString()).ToString("yyyy-MM-dd");
+                    adapter.InsertCommand = command;
+                    adapter.InsertCommand.ExecuteNonQuery();
+                    connection.Close();
+                    return new Dictionary<string, object>()
+                    {
+                        { "status", 204 },
+                        { "message", "The user was successfully updated." }
+                    };
+                }
+                else
+                {
+                    return new Dictionary<string, object>()
+                    {
+                        { "status", 404 },
+                        { "message", "No user exists with that UUID." }
+                    };
+                }
+            } catch(Exception ex)
+            {
+                return new ErrorResponse(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{uuid}")]
+        public ActionResult<object> DeleteUser(string uuid)
+        {
+            return new Dictionary<string, object>() { { "key", $"{uuid}" } };
         }
     }
 }
---
+++ models/ExistingUser.cs
@@ -0,0 +1,18 @@
+﻿namespace Restaurant_API.models
+{
+    public class ExistingUser
+    {
+        public string Uuid { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Dob { get; set; }
+        public ExistingUser() { }
+        public ExistingUser(string uuid, string name, string email, DateTime dob)
+        {
+            Uuid = uuid;
+            Name = name;
+            Email = email;
+            Dob = dob;
+        }
+    }
+}
